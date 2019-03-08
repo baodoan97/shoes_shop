fc8c58696090330e4ffa60334ba44426b91c1cff
ARYPHAMM <quoctienphamm@gmail.com>
2019-03-08 16:01:40 +0700

    users view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'users/sessions'}
   #user
   root 'pages#home'
   get 'contact', to: 'pages#contact'
