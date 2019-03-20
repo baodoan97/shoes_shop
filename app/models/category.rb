66636082e8907a898733f0d0eeb99abdb2e738b6
bpham1912 <bpham3442@gmail.com>
2019-03-20 13:34:24 +0700

    contact new

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ApplicationRecord
 	#associates
 	has_many :products
 	#validate
-		validates :name, presence:true, length: {minimum: 3, maximum: 15}, uniqueness:true
+		validates :name, presence:true, length: {minimum: 3, maximum: 30}, uniqueness:true
 		validates :description, presence:true, length: {minimum: 10, maximum: 200}, uniqueness:true
 end
 
