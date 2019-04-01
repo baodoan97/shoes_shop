e3573e0a6a17882a881773e0c68912820ff9a4d0
Geogre Doan <mr.cupo97@gmail.com>
2019-04-01 17:49:13 +0700

    edit index layout, jquery validate pay_type, add charge_id to payment to refund

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,50 +1,64 @@
 class PaymentsController < ApplicationController
 	def new
 		if current_cart.cart_products.size == 0
-	    	redirect_to '/', :notice => 'Your cart is empty'
-	     	return
-	    end
-        @payment = Payment.new
+    	redirect_to '/', :notice => 'Your cart is empty'
+     	return
+    end
+    @payment = Payment.new
 	end
 
 	def create
-        if current_cart.cart_products.size == 0
-            redirect_to '/', :notice => 'Your cart is empty'
-            return
+    if current_cart.cart_products.size == 0
+      redirect_to '/', :notice => 'Your cart is empty'
+      return
+    else
+  		@payment = Payment.new(payment_params)
+      if(params[:payment][:pay_type] == "atm")
+      # Stripe.api_key = "sk_test_Hm0ywtd94a4e27SHOfzVJLpZ"
+        @cart = current_cart
+        @amount = @cart.total_price.to_i * 100
+        token = params[:stripeToken]
+
+        # Create a Customer
+        customer = Stripe::Customer.create({
+          :description => params[:payment][:name], 
+          :card => token,
+        })
+
+        charge = Stripe::Charge.create({
+          :customer => customer.id,
+          :amount => @amount, # amount in cents, again
+          :currency => 'usd'
+        })
+        @payment.charge_id = charge.id
+        if @payment.save
+          @payment.add_line_items_from_cart(current_cart)
+          Cart.destroy(session[:cart_id])
+          session[:cart_id] = nil
+          @payment.payment_items.each do |item|
+              @product = Product.find(item.product_id)
+              @product.quantity = @product.quantity - item.quantity
+              @product.save
+          end
+          redirect_to root_path
         else
-    		@payment = Payment.new(payment_params)
-            # Stripe.api_key = "sk_test_Hm0ywtd94a4e27SHOfzVJLpZ"
-            @cart = current_cart
-            @amount = @cart.total_price.to_i * 100
-            token = params[:stripeToken]
-
-              # Create a Customer
-              customer = Stripe::Customer.create({
-                :description => params[:name], 
-                :card => token,
-            })
-
-              charge = Stripe::Charge.create({
-                :customer => customer.id,
-                :amount => @amount, # amount in cents, again
-                :currency => 'usd',
-                :description => params[:name],
-              })
-            if @payment.save
-                @payment.add_line_items_from_cart(current_cart)
-                Cart.destroy(session[:cart_id])
-                session[:cart_id] = nil
-                @payment.payment_items.each do |item|
-                    @product = Product.find(item.product_id)
-                    @product.quantity = @product.quantity - item.quantity
-                    @product.save
-                end
-                redirect_to root_path
-            else
-                render 'new'
-            end
+          render 'new'
         end
-	end
+      else
+        if @payment.save
+          @payment.add_line_items_from_cart(current_cart)
+          Cart.destroy(session[:cart_id])
+          session[:cart_id] = nil
+          @payment.payment_items.each do |item|
+              @product = Product.find(item.product_id)
+              @product.quantity = @product.quantity - item.quantity
+              @product.save
+          end
+          redirect_to root_path
+        end
+      end
+    end
+  end
 
     private
     def payment_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_155829) do
+ActiveRecord::Schema.define(version: 2019_03_30_145000) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 2019_03_25_155829) do
     t.string "address"
     t.integer "status"
     t.integer "pay_type"
+    t.string "charge_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
