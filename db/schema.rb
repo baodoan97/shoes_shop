9a6b9fee75bea563dc6c6e56b376153f6b35e503
Geogre Doan <mr.cupo97@gmail.com>
2019-04-07 20:14:22 +0700

    edit index template

--- db/schema.rb
+++ db/schema.rb
@@ -145,7 +145,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_143654) do
     t.string "charge_id"
   end
 
-  create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.float "latitude"
     t.float "longitude"
