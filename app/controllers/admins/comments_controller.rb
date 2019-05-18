754314bae4a411c59579dbc523917557f650aaf6
ARYPHAMM <quoctienphamm@gmail.com>
2019-05-18 10:42:47 +0700

    fix css received when admin page send comment user

--- app/controllers/admins/comments_controller.rb
+++ app/controllers/admins/comments_controller.rb
@@ -14,8 +14,13 @@ class Admins::CommentsController < BaseController
          
          @comment = nil
          @notification = nil
+         @check = false
          if params[:comment][:request] == 'delete'
-            Comment.find(params[:comment][:id].to_i).destroy
+            @comment =  Comment.find(params[:comment][:id].to_i).destroy
+            @notification = Notification.new
+            @notification.user = @comment.user
+            @notification.content = "violation of regulations comment on product #{@comment.product.name}"
+            @notification.save
          else
             @comment = Comment.find(params[:comment][:id].to_i)
             @comment.display = true 
@@ -25,6 +30,7 @@ class Admins::CommentsController < BaseController
             @notification.user = @comment.user
             @notification.content = "comment on product #{@comment.product.name} was browse"
             @notification.save
+            @check = true
          end
 
      
