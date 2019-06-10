9ca8be0ce69e6f3c5f632da1fa6a98804f93a7d6
ARYPHAMM <quoctienphamm@gmail.com>
2019-06-10 20:22:16 +0700

    resize image products

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,11 @@
 class Stock < ApplicationRecord
 	belongs_to :product
+	before_save :check_unique_size
+	def check_unique_size
+       if Product.find(self.product_id).stocks.find_by_size(self.size) != nil
+       	self.destroy
+           errors.add(:stocks, "have size #{self.size} present!!!")
+           return false
+       end
+	end
 end
