a964738104dcaa00c3183271c550be4eafaddf4d
ARYPHAMM <quoctienphamm@gmail.com>
2019-06-15 01:35:42 +0700

    fix nav , enum customer for product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_082404) do
+ActiveRecord::Schema.define(version: 2019_06_14_131413) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -203,6 +203,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_082404) do
     t.datetime "updated_at", null: false
     t.integer "liked", default: 0
     t.integer "brand_id"
+    t.integer "customer", default: 0
   end
 
   create_table "stocks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
