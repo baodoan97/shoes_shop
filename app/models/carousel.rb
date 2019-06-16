bdfc8a7c5bed697758ad7e0065fc2092274064be
ARYPHAMM <quoctienphamm@gmail.com>
2019-06-16 17:10:23 +0700

    fix some errors when update file , custom show image fit with size image

--- app/models/carousel.rb
+++ app/models/carousel.rb
@@ -3,21 +3,23 @@ class Carousel < ApplicationRecord
   has_one_attached :image
   validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
     dimension: { width: { min: 1920, max: 1920 },
-                 height: { min: 430, max: 430 }, message: 'is not given between dimension' }
+                 height: { min: 430, max: 430 }, message: 'is not given between dimension'
+                  }
     # validates :new_id , uniqueness: true
-  before_save :check_main,:check_news
+  before_save :check_news
+  after_save :check_main
   private
   def check_main
-    if Carousel.all.where(main: true).length == 1 && self.main == true
-      self.destroy
+    if Carousel.all.where(main: true).length == 2 && self.main == true
+      self.main = false
+      self.save
       errors.add(:main, "was have carousel top")
       return false
     end
   end
-
   def check_news
     begin
-      if New.find(self.new_id.to_i) == nil
+      if New.find(self.new_id.to_i) == nil || self.new_id != 0 
         self.destroy
         errors.add(:news, " id not exists")
         return false
